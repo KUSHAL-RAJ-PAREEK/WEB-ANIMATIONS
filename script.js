03b51d74228c669852655677ebab8edd7c741b73
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2025-06-12 10:35:44 +0530

    commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// gsap.to(".box1",{
+//     x:1500,
+//     rotate:360,
+//     duration:1.5,
+//     delay:1
+// })
+
+
+// gsap.to(".box2",{
+//     x:1500,
+//     backgroundColor:"yellow",
+//     duration:1.5,
+//     delay:2.5
+// })
+
+// gsap.to("box3",{
+//     x:1500,
+//     scale:0.5,
+//     duration:1,
+//     delay:4
+// })
+
+
+
+var tl = gsap.timeline()
+
+tl.to(".box1",{
+    x:1500,
+    rotate:360,
+    duration:1.5,
+    delay:1
+})
+
+
+tl.to(".box2",{
+    x:1500,
+    duration:1.5,
+
+})
