eb0f987c03da9dac48154f6da4aa247813ea46b3
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2025-06-12 18:03:18 +0530

    commit

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-var main = document.querySelector(".main")
 
-var cursor = document.querySelector(".cursor")
+var menu  = document.querySelector(".nav i")
+var cross = document.querySelector(".full i")
 
-var imageDiv = document.querySelector(".image");
+var tl = gsap.timeline()
+
+ tl.to(".full",{
+    right:0,
+    duration:0.6
+ })
 
 
-main.addEventListener('mousemove',(dets)=>{
-    gsap.to(cursor,{
-        x:dets.x,
-        y:dets.y,
-        duration:1,
-        ease:"back.out"
-    })
+ tl.from(".full h4",{
+    x:150,
+    duration: 0.7,
+    stagger:0.28,
+    opacity:0
+ })
+
+
+ tl.from(".full i",{
+    opacity:0
+ })
+
+ tl.pause()
+
+menu.addEventListener('click',()=>{
+    tl.play()
 })
 
-
-imageDiv.addEventListener('mouseenter',()=>{
-    cursor.innerHTML = "view more"
-    cursor.style.backgroundColor = " hsla(0, 0%, 100%, 0.447)"
-    gsap.to(cursor,{
-        scale:4
-    })
-})
-
-
-imageDiv.addEventListener('mouseleave',()=>{
-        cursor.innerHTML = ""
-            cursor.style.backgroundColor = "#fff"
-
-    gsap.to(cursor,{
-        scale: 1
-    })
+cross.addEventListener('click',()=>{
+    tl.reverse()
 })
